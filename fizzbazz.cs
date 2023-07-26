cfc871740ce6d03107695f8bf271f8cb89115453
Mario1199 <76882859+Mario1199@users.noreply.github.com>
2023-07-27 00:37:43 +0100

    FizzBazz
    using System;
    using System.Collections.Generic;
    using System.Linq;
    using System.Text;
    using System.Threading.Tasks;
    
    namespace FIZZBAZZ
    {
        class Program
        {
            static void Main(string[] args)
            {
                    // Defina o valor máximo (N) para o qual deseja executar o FizzBuzz
                    int N = 55;
    
                    for (int i = 1; i <= N; i++)
                    {
                        if (i % 3 == 0 && i % 5 == 0)
                        {
                            Console.WriteLine("FizzBuzz");
                               Console.ReadLine();
                        }
                        else if (i % 3 == 0)
                        {
                            Console.WriteLine("Fizz");
                             Console.ReadLine();
                    }
                        else if (i % 5 == 0)
                        {
                            Console.WriteLine("Buzz");
                             Console.ReadLine();
                    }
                        else
                        {
                            Console.WriteLine(i);
                            Console.ReadLine();
                    }
                    }
                }
            }
    
        
    }

---
+++ fizzbazz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIZZBAZZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                // Defina o valor máximo (N) para o qual deseja executar o FizzBuzz
+                int N = 55;
+
+                for (int i = 1; i <= N; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                           Console.ReadLine();
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                         Console.ReadLine();
+                }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                         Console.ReadLine();
+                }
+                    else
+                    {
+                        Console.WriteLine(i);
+                        Console.ReadLine();
+                }
+                }
+            }
+        }
+
+    
+}
+
